7ba314617705f3f41fb13c0b636f95e125ffe165
aishanoor1439 <aisha.noor1439@gmail.com>
2025-02-12 05:59:03 +0100

    Second Commit
    completed!

--- src/main/java/com/mycompany/mavenproject3/Mavenproject3.java
+++ src/main/java/com/mycompany/mavenproject3/Mavenproject3.java
@@ -1,16 +1,61 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.mavenproject3;
 
-/**
- *
- * @author ABC
- */
+import java.util.Scanner;
+
 public class Mavenproject3 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        System.out.println("Discount Calculator");
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter no of Items:");
+        int n = input.nextInt();
+        String[][] items = new String[n][3];
+
+//        Filling up the Array
+        for (int i = 0; i < n; i++) {
+//                Storing name
+            System.out.println("\nEnter details for Item " + i + 1 + ":");
+            System.out.println("Name: ");
+            items[i][0] = input.next();
+//                Storing price
+            System.out.println("Price: ");
+            items[i][1] = input.next();
+//                Storing quantity
+            System.out.println("Quantity: ");
+            items[i][2] = input.next();
+        }
+
+//        Typecasting price and quantity
+        double totalPrice = 0;
+        for (int i = 0; i < n; i++) {
+            double price = Double.valueOf(items[i][1]);
+            int quantity = Integer.valueOf(items[i][2]);
+            totalPrice += price * quantity;
+        }
+
+//        Applying discount
+        System.out.println("\nAre you from Bahria University? (yes/no): ");
+        String isFromBahria = input.next().toLowerCase();
+
+        double discount = 0;
+        if (isFromBahria == "yes") {
+            discount = 0.30;
+        } else if (totalPrice >= 50000 && totalPrice <= 100000) {
+            discount = 0.20;
+        } else if (totalPrice > 100000) {
+            discount = 0.30;
+        }
+
+//        Calculating final amount
+        double discountedAmount = totalPrice * discount;
+        double finalAmount = totalPrice - discountedAmount;
+
+//        Ouput
+        System.out.println("Total Price:" + totalPrice);
+        System.out.println("Discount Applied:" + discount * 100 + "%");
+        System.out.println("Discounted Amount:" + discountedAmount);
+        System.out.println("Final Price after Discount:" + finalAmount);
+
     }
 }
